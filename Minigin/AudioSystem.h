2b614a6493d6aff4bb5fe45110a304d018688639
LEEVGS <lee.vgs@hotmail.com>
2023-05-17 01:39:29 +0200

    Added logger

--- Minigin/AudioSystem.h
+++ Minigin/AudioSystem.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <memory>
 namespace dae
 {
 	using SoundId = unsigned short;
@@ -38,5 +39,16 @@ namespace dae
 		void StopAll() override {}
 		void PauseAll() override {}
 		void ResumeAll() override {}
+	};
+	class AudioSystemLogger final : public AudioSystem
+	{
+		std::unique_ptr<AudioSystem> m_logger;
+	public:
+		AudioSystemLogger(std::unique_ptr<AudioSystem> logger) { m_logger = std::move(logger); }
+		void Play(const SoundId id, const float volume) override { printf("Playing soud: %d with volume: %f\n", id, static_cast<double>(volume)); m_logger->Play(id, volume); }
+		void LoadSound(const SoundId id, const std::string& path) override { printf("Loading sound: %d with path: %s\n", id, path.c_str()); m_logger->LoadSound(id, path); }
+		void StopAll() override { printf("Stopping all sounds\n"); m_logger->StopAll(); }
+		void PauseAll() override { printf("Pausing all sounds\n"); m_logger->PauseAll(); }
+		void ResumeAll() override { printf("Resume all sounds\n"); m_logger->ResumeAll(); }
 	};
 }
--- TronBattleTanks/TronBattleTanks.cpp
+++ TronBattleTanks/TronBattleTanks.cpp
@@ -37,7 +37,7 @@ void Demo()
 	auto& sceneManager = dae::SceneManager::GetInstance();
 	auto& input = dae::InputManager::GetInstance();
 
-	dae::ServiceLocator::RegisterAudioSystem(std::make_unique<dae::AudioSystemSDL2>());
+	dae::ServiceLocator::RegisterAudioSystem(std::make_unique<dae::AudioSystemLogger>(std::move(std::make_unique<dae::AudioSystemSDL2>())));
 	dae::ServiceLocator::GetAudioSystem()->LoadSound(420, "Fart.mp3");
 	dae::ServiceLocator::GetAudioSystem()->LoadSound(69, "AH.wav");
 	dae::ServiceLocator::GetAudioSystem()->Play(420, 1.2f);
